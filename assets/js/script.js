4f05d6c8b8a9dcb712bfa9e638ff2a17e810c3ef
ghozi29 <ghoziskripsi@gmail.com>
2025-07-12 20:29:33 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Full Stack Internet of Things Developer", "Android Reparation Specialist", "Web Developer Enthusiast", "Machine Learning Enthusiast"],
+    strings: ["Automation Engineer, Full Stack Internet of Things Developer", "Android Reparation Specialist", "Web Developer Enthusiast", "Machine Learning Enthusiast"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
